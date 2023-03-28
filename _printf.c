4a9f9b652a00669c5f6db47f2df21510199dbd61
Chinyere Priscilla Ikeji <122909325+Ikeji23@users.noreply.github.com>
2023-03-28 10:07:58 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 void print_buffer(char buffer[], int *buff_ind);
-
 /**
  * _printf - printf function
- * @format: format
+ * @format: format.
  * Return: Printed chars.
  */
-int_print(const char *format, ...)
+int _printf(const char *format, ...)
 {
 	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
@@ -22,36 +21,34 @@ int_print(const char *format, ...)
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
 		if (format[i] != '%')
+		{
 			buffer[buff_ind++] = format[i];
-		if (buff_ind == BUFF_SIZE)
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
 			print_buffer(buffer, &buff_ind);
-		/* write(1, &format[i], 1);*/
-		printed_chars++;
-	}
-	else
-	{
-		print_buffer(buffer, &buff_ind);
-		flags = get_flags(format, &i);
-		width = get_width(format, &i, list);
-		precision = get_precision(format, &i, list);
-		size = get_size(format, &i);
-		++i;
-		printed = handle_print(format, &i, list, buffer,
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
 				flags, width, precision, size);
-		if (printed == -1)
-			return (-1);
-		printed_chars += printed;
-
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
 	}
 
-}
+	print_buffer(buffer, &buff_ind);
 
-print_buffer(buffer, &buff_ind);
+	va_end(list);
 
-va_end(list);
-
-return (printed_chars);
-
+	return (printed_chars);
 }
 
 /**
